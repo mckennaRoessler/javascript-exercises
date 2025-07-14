e2ecfe77f4e544c45e5de9353ebf267d0ed03ea2
McKenna Roessler <roesslerm135@gmail.com>
2025-07-14 11:53:26 -0500

    Complete exercises 3-5

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+    return newString;
 
 };
+
+const string = "hello";
+const newString = reverseString(string);
+console.log(newString);
+
+/*
+Alternatively, a more streamlined version would be:
+
+const reverseString = function (string) {
+  return string.split("").reverse().join("");
+};
+
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
