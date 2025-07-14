46983b267a3d45b11af3595e0072d120e18aa2b9
McKenna Roessler <roesslerm135@gmail.com>
2025-07-14 12:41:24 -0500

    Complete exercises 6 & 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divisibleByFour = year % 4 === 0;
+    const divisibleByOneHundred = year % 100 === 0;
+    const divisibleByFourHundred = year % 400 === 0;
 
+    if 
+    (
+        divisibleByFour &&
+        (!divisibleByOneHundred || divisibleByFourHundred)
+    )
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 };
 
 // Do not edit below this line
